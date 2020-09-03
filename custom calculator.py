1a52e79c902987e8e8fd7bfb82b29e91596521e3
oconda90 <70242240+oconda90@users.noreply.github.com>
2020-09-02 18:25:52 -0700

    Add files via upload

---
+++ custom calculator.py
@@ -0,0 +1,10 @@
+#hello, this is a calculator will tell you how many dollars you have based on the number of nickles and dimes you have
+print ("hello there! Welcome today we'll determine how much money you have!")
+Nickles = 5
+Dime = 10
+num1 = input("How many Nickles do you have?")
+#with this input ill determine the number of nickles the user has
+num2= input("How many dimes do you have?")
+result = int(num1) * Nickles + int(num2) * Dime
+print (result, "cents")
+#
